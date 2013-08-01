747bbaadc2794eef78b2376bed453f3554050f2b
Bennett Hoffman <bennett@coinharvest.com>
2013-08-01 00:06:55 -0700

    Use 2.1.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default[:play2][:revision]                   = "HEAD"
-default[:play2][:repository]                 = "git://github.com/playframework/Play20.git"
+default[:play2][:revision]                   = "2.1.2"
+default[:play2][:repository]                 = "https://github.com/playframework/playframework"
 
 default[:play2][:path][:prefix]              = "/srv"
 default[:play2][:path][:base]                = "#{default[:play2][:path][:prefix]}/play2"
